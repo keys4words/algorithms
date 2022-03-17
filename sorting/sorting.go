e4050342a5744ef866940f6e7d33975ce01bc1e8
keys4words <keys4words@gmail.com>
2022-03-17 17:11:52 +0300

    add merge sort

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -19,3 +19,41 @@ func SelectionSort(arr []int) {
 		}
 	}
 }
+
+func MergeSort(arr []int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+	middle := int(len(arr) / 2)
+	left := make([]int, middle)
+	right := make([]int, len(arr)-middle)
+	for i := 0; i < len(arr); i++ {
+		if i < middle {
+			left[i] = arr[i]
+		} else {
+			right[i-middle] = arr[i]
+		}
+	}
+	return merge(MergeSort(left), MergeSort(right))
+}
+func merge(left, right []int) (result []int) {
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+	return slice
+}
