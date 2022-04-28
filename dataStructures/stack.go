732eef5e01926b53091f9764f26b0e49a60c45b9
keys4words <keys4words@gmail.com>
2022-04-28 21:52:13 +0300

    add impl of stack

--- dataStructures/stack.go
+++ dataStructures/stack.go
@@ -1,23 +1,34 @@
-package datastructures
+package main
 
-type Stack []string
+import "fmt"
 
-func (s *Stack) IsEmpty() bool {
-	return len(*s) == 0
+type Stack struct {
+	items []int
 }
 
-func (s *Stack) Push(str string) {
-	*s = append(*s, str)
+func (s *Stack) Push(item int) {
+	s.items = append(s.items, item)
 }
 
-func (s *Stack) Pop() (string, bool) {
-	if s.IsEmpty() {
-		return "", false
-	} else {
-		index := len(*s) - 1
-		el := (*s)[index]
-		*s = (*s)[:index]
-		return el, true
+func (s *Stack) Pop() int {
+	left := len(s.items)
+	if left == 0 {
+		return -1
 	}
+	item, items := s.items[left-1], s.items[0:left-1]
+	s.items = items
+	return item
+}
 
+func main() {
+	s := Stack{}
+	s.Push(1)
+	s.Push(2)
+	s.Push(3)
+	s.Push(4)
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
 }
